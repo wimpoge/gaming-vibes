9b35e778c669fb2d58fe5626830dc1670a295756
wimpoge <34478110+wimpoge@users.noreply.github.com>
2022-04-27 01:03:03 +0700

    add auth netlify

---
+++ stores/authContext.js
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import netlifyIdentity  from "netlify-identity-widget";
+
+const AuthContext = createContext({
+    user: null,
+    login: () => {},
+    logout: () => {},
+    authReady: false
+});
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    useEffect (() => {
+        netlifyIdentity.on('login', (user) => {
+            setUser(user);
+            netlifyIdentity.close();
+            console.log('login');
+        });
+
+        //  init netlify identity connection
+        netlifyIdentity.init();
+    }, []);
+
+    const login = () => {
+        netlifyIdentity.open();
+    };
+
+    const context = { user, login }
+
+    return (
+        <AuthContext.Provider value={context}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
